9a098feb8da355219c1b01c68a757136a8e6293c
David Ingleby <david.ingleby@tutanota.com>
2022-12-26 00:25:20 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var electron_1 = require("electron");
+var dust_cloud_1 = require("./dust-cloud");
+dust_cloud_1.default.main(electron_1.app, electron_1.BrowserWindow);
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,4 @@
+import { app, BrowserWindow } from 'electron';
+import Main from './dust-cloud';
+
+Main.main(app, BrowserWindow);
